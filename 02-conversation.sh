c91b0f6f46006f3e03362dc86878993a05992979
Tharun Teja <tharunteja344@gmail.com>
2026-06-13 16:30:49 +0530

    conversation script added

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Trump:: Hey Mush, How are you?"
+echo "Musk:: Hello Trump, I am fine!"
+echo "Trump:: What are you doing?"
+echo "Musk:: Nothing, just going to Mars!"
+echo "Trump:: Okay carry on!"
