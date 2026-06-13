51740425d594994346b5d6ff30a4b98ee599c8f4
Tharun Teja <tharunteja344@gmail.com>
2026-06-13 17:48:40 +0530

    data types script added

---
+++ 06-data-types.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+#this is comment
+NUMBER1=100
+NUMBER2=200
+TIMESTAMP=$(date)
+echo "Scirpt executed at: $TIMESTAMP"
+SUM=$(( NUMBER1+NUMBER2 ))
+echo "Sum of $NUMBER1 and $NUMBER2 is: $SUM"
