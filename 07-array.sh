c9da7d896e5dcbfd2d0e3dc085f4782c1d745073
Tharun Teja <tharunteja344@gmail.com>
2026-06-13 17:53:49 +0530

    array script added

---
+++ 07-array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+MOVIES=("Court" "HIT3" "PUSHPA2" "Thandel")
+echo "First Movie: ${MOVIES[O]}"
+echo "Second Movie: ${MOVIES[1]}"
+echo "Third Movie: ${MOVIES[2]}"
+echo "Fourth Movie: ${MOVIES[3]}"
+echo "Fifth Movie: ${MOVIES[4]}"
+echo "All Movie: ${MOVIES[@]}"
+
