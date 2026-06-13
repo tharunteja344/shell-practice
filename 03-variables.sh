19d207d0e226526b3e878b49794309a2a2429296
Tharun Teja <tharunteja344@gmail.com>
2026-06-13 16:37:25 +0530

    Variables script added

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PERSON1=Modi
+PERSON2=Musk
+
+echo "$PERSON1:: Hey $PERSON2, How are you?"
+echo "$PERSON2:: Hello $PERSON1, I am fine!"
+echo "$PERSON1:: What are you doing?"
+echo "$PERSON2:: Nothing, just going to Mars!"
+echo "$PERSON1:: Okay carry on!"
